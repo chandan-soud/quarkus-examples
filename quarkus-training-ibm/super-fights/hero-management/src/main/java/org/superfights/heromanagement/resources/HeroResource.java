830d2d71218bf30b2a4e4fe14b42e23bd8529ecb
Chandan Soud <chandan.k.soud@gmail.com>
2020-08-20 18:54:44 +0200

    Added the rest apis for heros with dummy data

--- quarkus-training-ibm/super-fights/hero-management/src/main/java/org/superfights/heromanagement/resources/HeroResource.java
+++ quarkus-training-ibm/super-fights/hero-management/src/main/java/org/superfights/heromanagement/resources/HeroResource.java
@@ -1,17 +1,91 @@
 package org.superfights.heromanagement.resources;
 
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.jboss.logging.Logger;
+import org.superfights.heromanagement.entities.Hero;
+import org.superfights.heromanagement.services.HeroService;
 
 @Path("/api/heroes")
+@Produces(MediaType.APPLICATION_JSON)
 public class HeroResource {
 
-    @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello Heroes!";
-    }
+	private static final Logger LOGGER = Logger.getLogger(HeroResource.class);
+
+	@Inject
+	HeroService heroService;
+
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello Heroes!";
+	}
+
+	@GET
+	@Path("/random")
+	public Response getRandomHero() {
+		Hero hero = heroService.findRandomHero();
+		LOGGER.debug("Found random hero " + hero);
+		return Response.ok(hero).build();
+	}
+
+	@GET
+	public Response getAllHeroes() {
+		List<Hero> heroes = heroService.findAllHeroes();
+		LOGGER.debug("All heroes " + heroes);
+		return Response.ok(heroes).build();
+	}
+
+	@GET
+	@Path("/{id}")
+	public Response getHero(@PathParam("id") Long id) {
+		Hero hero = heroService.findHeroById(id);
+		if (hero != null) {
+			LOGGER.debug("Found hero " + hero);
+			return Response.ok(hero).build();
+		} else {
+			LOGGER.debug("No hero found with id " + id);
+			return Response.noContent().build();
+		}
+	}
+
+	@POST
+	public Response createHero(@Valid Hero hero, @Context UriInfo uriInfo) {
+		hero = heroService.persistHero(hero);
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(Long.toString(hero.id));
+		LOGGER.debug("New hero created with URI " + builder.build().toString());
+		return Response.created(builder.build()).build();
+	}
+
+	@PUT
+	public Response updateHero(@Valid Hero hero) {
+		hero = heroService.updateHero(hero);
+		LOGGER.debug("Hero updated with new values " + hero);
+		return Response.ok(hero).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response deleteHero(@PathParam("id") Long id) {
+		heroService.deleteHero(id);
+		LOGGER.debug("Hero deleted with id " + id);
+		return Response.noContent().build();
+	}
+
 }
---
+++ quarkus-training-ibm/super-fights/hero-management/src/main/java/org/superfights/heromanagement/services/HeroService.java
@@ -0,0 +1,55 @@
+package org.superfights.heromanagement.services;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+import javax.validation.Valid;
+
+import org.superfights.heromanagement.entities.Hero;
+
+@ApplicationScoped
+@Transactional(value = TxType.REQUIRED)
+public class HeroService {
+
+	@Transactional(value = TxType.SUPPORTS)
+	public List<Hero> findAllHeroes() {
+		return Hero.listAll();
+	}
+
+	@Transactional(value = TxType.SUPPORTS)
+	public Hero findHeroById(Long id) {
+		return Hero.findById(id);
+	}
+
+	@Transactional(value = TxType.SUPPORTS)
+	public Hero findRandomHero() {
+		Hero randomHero = null;
+		while (randomHero == null) {
+			randomHero = Hero.findRandomHero();
+		}
+		return randomHero;
+	}
+
+	public Hero persistHero(@Valid Hero hero) {
+		Hero.persist(hero);
+		return hero;
+	}
+
+	public Hero updateHero(@Valid Hero hero) {
+		Hero entity = Hero.findById(hero.id);
+		entity.name = hero.name;
+		entity.otherName = hero.otherName;
+		entity.level = hero.level;
+		entity.picture = hero.picture;
+		entity.powers = hero.powers;
+		return entity;
+	}
+
+	public void deleteHero(Long id) {
+		Hero hero = Hero.findById(id);
+		hero.delete();
+	}
+
+}

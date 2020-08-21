9c6abed78b5d7fca6dcc34a180e1749b8c0bf512
Chandan-Soud <chandan.k.soud@gmail.com>
2020-08-21 19:07:29 +0200

    Added openApi and swagger config

--- quarkus-training-ibm/super-fights/villain-management/src/main/java/org/superfights/villainmanagement/resources/VillainResource.java
+++ quarkus-training-ibm/super-fights/villain-management/src/main/java/org/superfights/villainmanagement/resources/VillainResource.java
@@ -1,7 +1,9 @@
 package org.superfights.villainmanagement.resources;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -14,15 +16,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 import org.superfights.villainmanagement.entities.Villain;
 import org.superfights.villainmanagement.services.VillainService;
 
+@Tag(ref = "villains")
 @Path("/api/villains")
 @Produces(APPLICATION_JSON)
 public class VillainResource {
@@ -32,13 +40,18 @@ public class VillainResource {
 	@Inject
 	VillainService villainService;
 
+	@Operation(summary = "Returns \"Hello Villains!\" string, serves as a dummy api")
+	@APIResponse(responseCode = "200", content = @Content(mediaType = TEXT_PLAIN, schema = @Schema(implementation = String.class)))
+	@Tag(ref = "generic")
 	@GET
 	@Path("/hello")
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(TEXT_PLAIN)
 	public String hello() {
 		return "Hello Villains!";
 	}
 
+	@Operation(summary = "Returns a random villain")
+	@APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Villain.class, required = true)))
 	@GET
 	@Path("/random")
 	public Response getRandomVillain() {
@@ -47,6 +60,9 @@ public class VillainResource {
 		return Response.ok(villain).build();
 	}
 
+	@Operation(summary = "Returns all the villains from the database")
+	@APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Villain.class, type = SchemaType.ARRAY)))
+	@APIResponse(responseCode = "204", description = "No villains")
 	@GET
 	public Response getAllVillains() {
 		List<Villain> villains = villainService.findAllVillains();
@@ -54,6 +70,9 @@ public class VillainResource {
 		return Response.ok(villains).build();
 	}
 
+	@Operation(summary = "Returns a villain for a given identifier")
+	@APIResponse(responseCode = "200", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Villain.class)))
+	@APIResponse(responseCode = "204", description = "The villain is not found for a given identifier")
 	@GET
 	@Path("/{id}")
 	public Response getVillain(@PathParam("id") Long id) {
@@ -67,6 +86,8 @@ public class VillainResource {
 		}
 	}
 
+	@Operation(summary = "Creates a valid villain")
+	@APIResponse(responseCode = "201", description = "The URI of the created villain", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = URI.class)))
 	@POST
 	public Response createVillain(@Valid Villain villain, @Context UriInfo uriInfo) {
 		villain = villainService.persistVillain(villain);
@@ -75,6 +96,8 @@ public class VillainResource {
 		return Response.created(builder.build()).build();
 	}
 
+	@Operation(summary = "Updates an exiting  villain")
+	@APIResponse(responseCode = "200", description = "The updated villain", content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Villain.class)))
 	@PUT
 	public Response updateVillain(@Valid Villain villain) {
 		villain = villainService.updateVillain(villain);
@@ -82,6 +105,8 @@ public class VillainResource {
 		return Response.ok(villain).build();
 	}
 
+	@Operation(summary = "Deletes an exiting villain")
+	@APIResponse(responseCode = "204")
 	@DELETE
 	@Path("/{id}")
 	public Response deleteVillain(@PathParam("id") Long id) {

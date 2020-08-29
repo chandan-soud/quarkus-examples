d527773159bb33bc755ac1c99f0142cb77ee74df
chandan-soud <chandan.k.soud@gmail.com>
2020-08-29 17:42:17 +0200

    Added first version of statistic-management

---
+++ quarkus-training/super-fights/statistic-management/src/test/java/org/superfights/statisticmanagement/resources/NativeStatisticResourceIT.java
@@ -0,0 +1,9 @@
+package org.superfights.statisticmanagement.resources;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStatisticResourceIT extends StatisticResourceTest {
+
+    // Execute the same tests but in native mode.
+}
